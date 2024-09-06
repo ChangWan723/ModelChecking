5ec4bbe5becd4f3deb70dc9c4bfd5f76e5c35502
ChangWan <wan723@outlook.com>
2024-09-06 20:14:18 +0100

    modify

--- src/tests/project/jpf/NumericTest.java
+++ src/tests/project/jpf/NumericTest.java
@@ -4,23 +4,20 @@ import gov.nasa.jpf.util.test.TestJPF;
 
 
 public class NumericTest extends TestJPF {
-    static class InterestCalculator {
-        void calculateCompoundInterest() {
-            double monthlyRate = 0.3;
+    static class C2 {
+        void doSomething() {
+            double x = 0.5;
         }
     }
 
     @org.junit.Test
     public void testVars() {
-        // A bug was found when using NumericValueChecker.
-        // Here's a simple example of the bug
         if (verifyNoPropertyViolation("+listener=.listener.NumericValueChecker",
-                "+range.vars=monthlyRate",
-                "+range.monthlyRate.var=*$InterestCalculator.calculateCompoundInterest():monthlyRate",
-                "+range.monthlyRate.min=0",
-                "+range.monthlyRate.max=0.5")) {
-            InterestCalculator interestCalculator = new InterestCalculator();
-            interestCalculator.calculateCompoundInterest();
+                "+range.vars=x",
+                "+range.x.var=*$C2.doSomething():x",
+                "+range.x.max=0.9")) {
+            C2 c2 = new C2();
+            c2.doSomething();
         }
     }
 }
--- src/tests/project/scenario/UserScenario.java
+++ src/tests/project/scenario/UserScenario.java
@@ -7,6 +7,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.CountDownLatch;
 import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class UserScenario {
     private static final UserCtrl userCtrl = new UserCtrl();
